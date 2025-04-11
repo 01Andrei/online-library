bb3df037123ceb4ca4d4c1cb1c284ef0334db39a
01Andrei <job.kosteev@gmail.com>
2025-04-11 23:44:19 +0300

    feat: add Catalog and basic styles

---
+++ src/fakeBooks.js
@@ -0,0 +1,29 @@
+const fakeBooks = [
+    {
+      id: '1',
+      title: '1984',
+      author: 'George Orwell',
+      cover: 'https://placehold.co/200x300?text=1984',
+    },
+    {
+      id: '2',
+      title: 'Brave New World',
+      author: 'Aldous Huxley',
+      cover: 'https://placehold.co/200x300?text=Brave+New+World',
+    },
+    {
+      id: '3',
+      title: 'Fahrenheit 451',
+      author: 'Ray Bradbury',
+      cover: 'https://placehold.co/200x300?text=Fahrenheit+451',
+    },
+    {
+      id: '4',
+      title: 'Dune',
+      author: 'Frank Herbert',
+      cover: 'https://placehold.co/200x300?text=Dune',
+    },
+  ];
+  
+  export default fakeBooks;
+  
