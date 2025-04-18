f704d01966a522a3286fbef09c17a3d237750d43
01Andrei <job.kosteev@gmail.com>
2025-04-18 23:22:45 +0300

    feat: add persistance to favorites store using zustand middleware

--- src/store/favoritesStore.js
+++ src/store/favoritesStore.js
@@ -1,16 +1,23 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-export const useFavoritesStore = create((set, get) => ({
-  favorites: [],
-  toggleFavorite: (book) => {
-    const favorites = get().favorites;
-    const isAlready = favorites.find((fav) => fav.id === book.id);
+export const useFavoritesStore = create(
+  persist(
+    (set, get) => ({
+      favorites: [],
+      toggleFavorite: (book) => {
+        const favorites = get().favorites;
+        const isAlready = favorites.some((fav) => fav.id === book.id);
 
-    set({
-      favorites: isAlready
-        ? favorites.filter((fav) => fav.id !== book.id)
-        : [...favorites, book],
-    });
-  },
- 
-}));
+        set({
+          favorites: isAlready
+            ? favorites.filter((fav) => fav.id !== book.id)
+            : [...favorites, book],
+        });
+      },
+    }),
+    {
+      name: 'favorites-storage', // ключ в localStorage
+    }
+  )
+);
